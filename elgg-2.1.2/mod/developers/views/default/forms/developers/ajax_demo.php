208dfdbd38841ffa14c0b83cdb30267b3150461f
ncantu <nicolas.cantu@00smartvillage.com>
2016-06-16 12:05:05 +0200

    no elgg

--- elgg-2.1.2/mod/developers/views/default/forms/developers/ajax_demo.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This will be fetched via ajax by the developers/ajax_example AMD module
-
-echo "form demo";
--- elgg-2.1.2/vendor/elgg/elgg/mod/messages/views/default/messages/js.php
+++
@@ -1,7 +0,0 @@
-//<script>
-// messages plugin toggle
-elgg.register_hook_handler('init', 'system', function() {
-	$("#messages-toggle").click(function() {
-		$('input[type=checkbox]').click();
-	});
-});
